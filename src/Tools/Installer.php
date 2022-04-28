4bcef7483b44417e05e4148696307324351d9fb2
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-04-28 22:33:35 +0300

    -

--- src/Tools/Installer.php
+++ src/Tools/Installer.php
@@ -25,10 +25,15 @@ class Installer
     {
 
         $modules = self::_loadConfig($file);
-        foreach($modules['entries'] as $entry) {
-            if($entry['name'] === 'Tools') {
-                return;
+        if(is_array($modules['entries'])) {
+            foreach($modules['entries'] as $entry) {
+                if($entry['name'] === 'Tools') {
+                    return;
+                }
             }
+        }
+        else {
+            $modules['entries'] = [];
         }
 
         $modules['entries'][] = [
