128f82beeeb9261e1e5dc907c105f5385b8fb5ce
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-04-28 22:05:05 +0300

    -

--- src/Tools/Installer.php
+++ src/Tools/Installer.php
@@ -5,6 +5,46 @@ namespace App\Modules\Tools;
 
 class Installer
 {
+    private static function _loadConfig($file): array
+    {
+        return yaml_parse_file($file);
+    }
+
+    private static function _saveConfig($file, $config): void
+    {
+        yaml_emit_file($file, $config, \YAML_UTF8_ENCODING, \YAML_ANY_BREAK);
+    }
+
+    private static function _getMode($file): string
+    {
+        $appConfig = self::_loadConfig($file);
+        return $appConfig['mode'];
+    }
+
+    private static function _injectIntoModuleConfig($file): void
+    {
+
+        $modules = self::_loadConfig($file);
+        foreach($modules['entries'] as $entry) {
+            if($entry['name'] === 'MainFrame') {
+                return;
+            }
+        }
+
+        $modules['entries'][] = [
+            'name' => 'Tools',
+            'entry' => '\Tools\Module',
+            'desc' => 'Инструменты',
+            'enabled' => true,
+            'visible' => false,
+            'for' => ['manage'],
+            'config' => 'include(/config/tools.yaml)'
+        ];
+
+        self::_saveConfig($file, $modules);
+
+    }
+
     private static function _copyOrSymlink($mode, $pathFrom, $pathTo, $fileFrom, $fileTo): void 
     {
         print_r('Копируем '.$mode.' '.$pathFrom.' '.$pathTo.' '.$fileFrom.' '.$fileTo."\n");
@@ -42,6 +82,11 @@ class Installer
         print_r('Установка и настройка модуля Colibri Tools Module'."\n");
 
         $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir').'/';
+        $operation = $event->getOperation();
+        $installedPackage = $operation->getPackage();
+        $targetDir = $installedPackage->getName();
+        $path = $vendorDir.$targetDir;
+        $configPath = $path.'/src/Tools/config-template/';
         $configDir = './config/';
 
         if(!file_exists($configDir.'app.yaml')) {
@@ -49,57 +94,20 @@ class Installer
             return;
         }
 
-        $mode = 'dev';
-        $appYamlContent = file_get_contents($configDir.'app.yaml');
-        if(preg_match('/mode: (\w+)/', $appYamlContent, $matches) >=0 ) {
-            $mode = $matches[1];
-        }
-
-        $operation = $event->getOperation();
-        $installedPackage = $operation->getPackage();
-        $targetDir = $installedPackage->getName();
-        $path = $vendorDir.$targetDir;
-        $configPath = $path.'/src/Tools/config-template/';
+        $mode = self::_getMode($configDir.'app.yaml');
         
         // копируем конфиг
-        print_r('Копируем файл конфигурации'."\n");
-        if(file_exists($configDir.'tools.yaml')) {
-            print_r('Файл конфигурации найден, пропускаем настройку'."\n");
-            return;
-        }
+        print_r('Копируем файлы конфигурации'."\n");
         self::_copyOrSymlink($mode, $configPath, $configDir, 'module-'.$mode.'.yaml', 'tools.yaml');
-
-        if(file_exists($configDir.'tools-storages.yaml')) {
-            print_r('Файл конфигурации найден, пропускаем настройку'."\n");
-            return;
-        }
         self::_copyOrSymlink($mode, $configPath, $configDir, 'tools-storages.yaml', 'tools-storages.yaml');
 
-        // нужно прописать в модули
-        $modulesTargetPath = $configDir.'modules.yaml';
-        $modulesConfigContent = file_get_contents($modulesTargetPath);
-        if(strstr($modulesConfigContent, '- name: Tools') !== false) {
-            print_r('Модуль сконфигурирован, пропускаем'."\n");
-            return;
-        }
-
-        $modulesConfigContent = $modulesConfigContent.'
-  - name: Tools
-    entry: \Tools\Module
-    enabled: true
-    desc: Инструменты
-    visible: true
-    for:
-      - manage
-    config: include(/config/tools.yaml)';
-        file_put_contents($modulesTargetPath, $modulesConfigContent);
+        print_r('Встраиваем модуль'."\n");
+        self::_injectIntoModuleConfig($configDir.'modules.yaml');
 
         print_r('Установка скриптов'."\n");
-        $scriptsPath = $path.'/src/Tools/bin/';
-        $binDir = './bin/';
+        self::_copyOrSymlink($mode, $path.'/src/Tools/bin/', './bin/', 'tools-migrate.sh', 'tools-migrate.sh');
+        self::_copyOrSymlink($mode, $path.'/src/Tools/bin/', './bin/', 'tools-models-generate.sh', 'tools-models-generate.sh');
 
-        self::_copyOrSymlink($mode, $scriptsPath, $binDir, 'tools-migrate.sh', 'tools-migrate.sh');
-        self::_copyOrSymlink($mode, $scriptsPath, $binDir, 'tools-models-generate.sh', 'tools-models-generate.sh');
         print_r('Установка завершена'."\n");
 
     }
