616a69d7366be45bafadbf097141b66e2cca58ad
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-05-10 23:03:30 +0300

    -

--- src/Tools/bin/tools-migrate.sh
+++ src/Tools/bin/tools-migrate.sh
@@ -1,2 +1,9 @@
-# скрипт должен находится в папке <approot>/bin
+#!/bin/sh
+
+CURDIR=$(dirname $0)
+if [ "$CURDIR" = '.' ]
+then
+    CURDIR=`pwd`
+fi
+cd $CURDIR
 cd ../web && /usr/bin/php index.php localhost / command=migrate
