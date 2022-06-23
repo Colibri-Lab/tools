8d6a76687e726580cf4eab79a3b2b74c9f99333e
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2022-06-23 15:23:51 +0000

    -

---
+++ src/Tools/.Bundle/15.BackupPage.js
@@ -0,0 +1,11 @@
+App.Modules.Tools.BackupPage = class extends Colibri.UI.Component 
+{
+
+    constructor(name, container) {
+        super(name, container, Colibri.UI.Templates['App.Modules.Tools.BackupPage']);
+
+        this.AddClass('app-backup-page-component'); 
+ 
+    }
+
+}
